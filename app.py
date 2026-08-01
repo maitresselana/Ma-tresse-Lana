0dacd40ddeebb1d1275dec185567831566507ae8
maitresselana <maitresselanaftt@gmail.com>
2026-08-01 06:32:52 +0200

    Set debug mode to False in app.run
    Change debug mode to False for production.

--- app.py
+++ app.py
@@ -382,4 +382,4 @@ def delete_block(block_id):
     return redirect(url_for("admin"))
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT","5000")), debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT","5000")), debug=False)
